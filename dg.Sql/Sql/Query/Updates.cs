968529086222d317cc11ebabbbde94561c1e5080
Daniel Cohen Gindi <danielgindi@gmail.com>
2015-09-18 13:15:13 +0300

    Bugfix: UPDATE mode was not set by a call to UpdateFromOtherColumn

--- dg.Sql/Sql/Query/Updates.cs
+++ dg.Sql/Sql/Query/Updates.cs
@@ -13,6 +13,7 @@ namespace dg.Sql
         {
             return Update(columnName, value, false);
         }
+
         public Query Update(string columnName, object value, bool columnValueIsLiteral)
         {
             QueryMode currentMode = this.QueryMode;
@@ -30,16 +31,19 @@ namespace dg.Sql
             _ListInsertUpdate.Add(new AssignmentColumn(null, columnName, null, value, columnValueIsLiteral ? ValueObjectType.Literal : ValueObjectType.Value));
             return this;
         }
+
         public Query UpdateFromOtherColumn(string tableName, string columnName, string fromTableName, string fromTableColumn)
         {
             QueryMode currentMode = this.QueryMode;
-            if (currentMode != QueryMode.Update &&
-                currentMode != QueryMode.Insert &&
-                currentMode != QueryMode.InsertOrUpdate &&
-                _ListInsertUpdate != null)
+            if (currentMode != QueryMode.Update)
             {
-                _ListInsertUpdate.Clear();
                 this.QueryMode = QueryMode.Update;
+                if (currentMode != QueryMode.Insert &&
+                    currentMode != QueryMode.InsertOrUpdate &&
+                    _ListInsertUpdate != null)
+                {
+                    if (_ListInsertUpdate != null) _ListInsertUpdate.Clear();
+                }
             }
             if (_ListInsertUpdate == null) _ListInsertUpdate = new AssignmentColumnList();
             _ListInsertUpdate.Add(new AssignmentColumn(tableName, columnName, fromTableName, fromTableColumn, ValueObjectType.ColumnName));
