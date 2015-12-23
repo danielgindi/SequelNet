c86effe2edb0763655b323772993036bb23fe99e
Daniel Cohen Gindi <danielgindi@gmail.com>
2015-12-23 22:13:05 +0200

    Version updated to 1.0.0.45

--- dg.Sql.Connector.OleDb/Properties/AssemblyInfo.cs
+++ dg.Sql.Connector.OleDb/Properties/AssemblyInfo.cs
@@ -34,5 +34,5 @@ using System.Security;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.44")]
-[assembly: AssemblyFileVersion("1.0.0.44")]
+[assembly: AssemblyVersion("1.0.0.45")]
+[assembly: AssemblyFileVersion("1.0.0.45")]
