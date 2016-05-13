fc151e4fc42ac038b8f3b23a0b491417549d66a6
Daniel Cohen Gindi <danielgindi@gmail.com>
2016-05-13 16:06:08 +0300

    Added new convenience Join overloads

--- dg.Sql/Sql/Query/Joins.cs
+++ dg.Sql/Sql/Query/Joins.cs
@@ -52,5 +52,89 @@ namespace dg.Sql
             _ListJoin.Add(join);
             return this;
         }
+                
+        public Query LeftJoin(
+            TableSchema rightTableSchema, string rightTableAlias,
+            params JoinColumnPair[] pairs)
+        {
+            return Join(JoinType.LeftJoin, rightTableSchema, rightTableAlias, pairs);
+        }
+
+        public Query LeftJoin(JoinType joinType,
+            object rightTableSql, string rightTableAlias,
+            params JoinColumnPair[] pairs)
+        {
+            return Join(JoinType.LeftJoin, rightTableSql, rightTableAlias, pairs);
+        }
+
+        public Query RightJoin(
+            TableSchema rightTableSchema, string rightTableAlias,
+            params JoinColumnPair[] pairs)
+        {
+            return Join(JoinType.RightJoin, rightTableSchema, rightTableAlias, pairs);
+        }
+
+        public Query RightJoin(JoinType joinType,
+            object rightTableSql, string rightTableAlias,
+            params JoinColumnPair[] pairs)
+        {
+            return Join(JoinType.RightJoin, rightTableSql, rightTableAlias, pairs);
+        }
+
+        public Query InnerJoin(
+            TableSchema rightTableSchema, string rightTableAlias,
+            params JoinColumnPair[] pairs)
+        {
+            return Join(JoinType.InnerJoin, rightTableSchema, rightTableAlias, pairs);
+        }
+
+        public Query InnerJoin(JoinType joinType,
+            object rightTableSql, string rightTableAlias,
+            params JoinColumnPair[] pairs)
+        {
+            return Join(JoinType.InnerJoin, rightTableSql, rightTableAlias, pairs);
+        }
+
+        public Query LeftOuterJoin(
+            TableSchema rightTableSchema, string rightTableAlias,
+            params JoinColumnPair[] pairs)
+        {
+            return Join(JoinType.LeftOuterJoin, rightTableSchema, rightTableAlias, pairs);
+        }
+
+        public Query LeftOuterJoin(JoinType joinType,
+            object rightTableSql, string rightTableAlias,
+            params JoinColumnPair[] pairs)
+        {
+            return Join(JoinType.LeftOuterJoin, rightTableSql, rightTableAlias, pairs);
+        }
+
+        public Query RightOuterJoin(
+            TableSchema rightTableSchema, string rightTableAlias,
+            params JoinColumnPair[] pairs)
+        {
+            return Join(JoinType.RightOuterJoin, rightTableSchema, rightTableAlias, pairs);
+        }
+
+        public Query RightOuterJoin(JoinType joinType,
+            object rightTableSql, string rightTableAlias,
+            params JoinColumnPair[] pairs)
+        {
+            return Join(JoinType.RightOuterJoin, rightTableSql, rightTableAlias, pairs);
+        }
+
+        public Query FullOuterJoin(
+            TableSchema rightTableSchema, string rightTableAlias,
+            params JoinColumnPair[] pairs)
+        {
+            return Join(JoinType.FullOuterJoin, rightTableSchema, rightTableAlias, pairs);
+        }
+
+        public Query FullOuterJoin(JoinType joinType,
+            object rightTableSql, string rightTableAlias,
+            params JoinColumnPair[] pairs)
+        {
+            return Join(JoinType.FullOuterJoin, rightTableSql, rightTableAlias, pairs);
+        }
     }
 }
