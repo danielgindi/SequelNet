819bc86adc7ddb54f10b9115665f6f9b3d006c3b
Daniel Cohen Gindi <danielgindi@gmail.com>
2020-08-10 08:47:55 +0300

    Fixed Migration class forcing override of methods

--- SequelNet/Migrations/Migration.cs
+++ SequelNet/Migrations/Migration.cs
@@ -21,11 +21,17 @@
         /// <summary>
         /// What to do when migrating up to this version?
         /// </summary>
-        public abstract System.Threading.Tasks.Task UpAsync();
+        public virtual System.Threading.Tasks.Task UpAsync()
+        {
+            return System.Threading.Tasks.Task.CompletedTask;
+        }
 
         /// <summary>
         /// What to do when migrating down from this version?
         /// </summary>
-        public abstract System.Threading.Tasks.Task DownAsync();
+        public virtual System.Threading.Tasks.Task DownAsync()
+        {
+            return System.Threading.Tasks.Task.CompletedTask;
+        }
     }
 }
