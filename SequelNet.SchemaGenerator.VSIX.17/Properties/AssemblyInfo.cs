aa76cf63b12ffe20afeefe473d2c5d7b00eccded
Daniel Cohen Gindi <danielgindi@gmail.com>
2026-09-10 11:54:40 +0300

    3.0.163

--- SequelNet.SchemaGenerator.VSIX.17/Properties/AssemblyInfo.cs
+++ SequelNet.SchemaGenerator.VSIX.17/Properties/AssemblyInfo.cs
@@ -15,5 +15,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: NeutralResourcesLanguage("en-US")]
-[assembly: AssemblyVersion("3.0.162")]
-[assembly: AssemblyFileVersion("3.0.162")]
+[assembly: AssemblyVersion("3.0.163")]
+[assembly: AssemblyFileVersion("3.0.163")]
